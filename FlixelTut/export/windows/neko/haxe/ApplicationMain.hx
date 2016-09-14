c56d26b94eae54773f8b6c797f3807f0cc439429
Kyle Ring <ringk2@rpi.edu>
2016-09-14 16:23:26 -0400

    Added a call for an interact function (does nothing yet)

--- FlixelTut/export/windows/neko/haxe/ApplicationMain.hx
+++ FlixelTut/export/windows/neko/haxe/ApplicationMain.hx
@@ -511,7 +511,7 @@ class ApplicationMain {
 		
 		config = {
 			
-			build: "16",
+			build: "4",
 			company: "HaxeFlixel",
 			file: "FlixelTut",
 			fps: 60,
--- FlixelTut/export/windows/neko/haxe/lime/AssetData.hx
+++ FlixelTut/export/windows/neko/haxe/lime/AssetData.hx
@@ -16,16 +16,8 @@ class AssetData {
 		
 		if (!initialized) {
 			
-			path.set ("assets/data/data-goes-here.txt", "assets/data/data-goes-here.txt");
-			type.set ("assets/data/data-goes-here.txt", Reflect.field (AssetType, "text".toUpperCase ()));
 			path.set ("assets/images/duck.png", "assets/images/duck.png");
 			type.set ("assets/images/duck.png", Reflect.field (AssetType, "image".toUpperCase ()));
-			path.set ("assets/images/images-go-here.txt", "assets/images/images-go-here.txt");
-			type.set ("assets/images/images-go-here.txt", Reflect.field (AssetType, "text".toUpperCase ()));
-			path.set ("assets/music/music-goes-here.txt", "assets/music/music-goes-here.txt");
-			type.set ("assets/music/music-goes-here.txt", Reflect.field (AssetType, "text".toUpperCase ()));
-			path.set ("assets/sounds/sounds-go-here.txt", "assets/sounds/sounds-go-here.txt");
-			type.set ("assets/sounds/sounds-go-here.txt", Reflect.field (AssetType, "text".toUpperCase ()));
 			path.set ("flixel/sounds/beep.ogg", "flixel/sounds/beep.ogg");
 			type.set ("flixel/sounds/beep.ogg", Reflect.field (AssetType, "sound".toUpperCase ()));
 			path.set ("flixel/sounds/flixel.ogg", "flixel/sounds/flixel.ogg");
