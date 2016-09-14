7488519fb3d942055562e435ac1c90f473c6c055
Rikusaber <huange2@rpi.edu>
2016-09-14 16:58:33 -0400

    updated files

--- FlixelTut/source/MenuState.hx
+++ FlixelTut/source/MenuState.hx
@@ -6,23 +6,29 @@ import flixel.FlxState;
 import flixel.text.FlxText;
 import flixel.ui.FlxButton;
 import flixel.math.FlxMath;
+import flixel.tile.FlxTilemap;
 
 class MenuState extends FlxState
 {
+	
 	var _playButton:FlxButton;
 	override public function create():Void
 	{
-		//testing I placed hello world back
-		add(new FlxText(10,10,100,"Hello, world!"));
+		//add(new FlxText(10,10,100,"Hello, world!"));
 		_playButton = new FlxButton(0, 0, "Play", clickPlay);
 		_playButton.screenCenter();
 		add(_playButton);
 		super.create();
+		
+		
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		if (FlxG.keys.justPressed.ENTER)
+            FlxG.fullscreen = !FlxG.fullscreen;
+
 	}
 	function clickPlay():Void{
 		//switch to play scene
--- FlixelTut/source/PlayState.hx
+++ FlixelTut/source/PlayState.hx
@@ -12,13 +12,24 @@ class PlayState extends FlxState
 	var _player:Player;
 	override public function create():Void
 	{
-		_player = new Player(20, 20);
+		var bg = new FlxSprite(0, 0, "assets/images/WorldMap-Neighborhood.png");
+		bg.scale.set(.5, .5);
+		_player = new Player(700, 700);
+		_player.scale.set(.5, .5);
+		add(bg);
 		add(_player);
+		
 		super.create();
+		FlxG.camera.follow(_player, TOPDOWN, 1);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+		if (FlxG.keys.justPressed.ENTER){
+            FlxG.fullscreen = !FlxG.fullscreen;
+		}
+		
+
 	}
 }
